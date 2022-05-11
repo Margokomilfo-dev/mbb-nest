7be730c8669899d8a3e07f2b6f325ff41fc0243c
Margokomilfo-dev <margokomilfo@mail.ru>
2022-05-11 17:52:51 +0200

    added nodejs/cli -g

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
+
   app.enableCors({
     origin: ['http://localhost:3000', 'http://localhost:3001'],
   });
