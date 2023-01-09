1a2a9cb4e73d0ca49fb6afbcdc55211e1c3e2864
Margo <margokomilfo.dev@gmail.com>
2023-01-09 17:31:42 +0100

    changed port

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-const PORT = process.env.PORT || 5555;
+const port = process.env.PORT || 5555;
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
@@ -15,8 +15,8 @@ async function bootstrap() {
       'https://mbb-react.herokuapp.com',
     ],
   });
-  await app.listen(PORT, () => {
-    console.log('App listen port: ', PORT);
+  await app.listen(port, () => {
+    console.log('App listen port: ', port);
   });
 }
 
